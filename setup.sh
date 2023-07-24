4da17ba8e48678ed1e86230d0bcf20ee7a5b52c1
burstknight <burstknight@gmail.com>
2023-07-24 17:51:28 +0800

    feat: Update shell file to setup `.bashrc`
    - Target:
      The shell file `.bashrc` should be changed to help us easily use bash.
    
    - Changed:
      Update the shell file `setup.sh`.

--- setup.sh
+++ setup.sh
@@ -9,3 +9,23 @@ python -m pip install --upgrade pip
 pip install virtualenv
 echo "================== Finish =================="
 
+echo "Setup .bashrc ..."
+if ! [ -f "${HOME}/.bashrc" ]; then
+	touch ${HOME}/.bashrc
+fi
+
+echo "export LESS_TERMCAP_mb=$'\e[1;32m'" >> ${HOME}/.bashrc
+echo "export LESS_TERMCAP_md=$'\e[1;32m'" >> ${HOME}/.bashrc
+echo "export LESS_TERMCAP_me=$'\e[1;0m'" >> ${HOME}/.bashrc
+echo "export LESS_TERMCAP_se=$'\e[1;0m'" >> ${HOME}/.bashrc
+echo "export LESS_TERMCAP_so=$'\e[01;33m'" >> ${HOME}/.bashrc
+echo "export LESS_TERMCAP_ue=$'\e[0m'" >> ${HOME}/.bashrc
+echo "export LESS_TERMCAP_us=$'\e[1;4;31m'" >> ${HOME}/.bashrc
+echo ""
+
+echo "alias ll='ls -alF --color=auto'" >> ${HOME}/.bashrc
+echo "alias ls='ls --color=auto'" >> ${HOME}/.bashrc
+
+source ${HOME}/.bashrc
+
+echo "================== Finish =================="
