62c129942207d3ab539dbf0b15b92f923dedd2ec
burstknight <burstknight@gmail.com>
2024-07-22 13:07:16 +0800

    feat: Update `setup.sh` for msys2 packages
    - Target:
      The shell file `setup.sh` need be updated to change the necessary
      packages for msys2.
    
    - Changed:
      Update the shell file `setup.sh` for the msys2 packages.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 echo "Installing necesary packages and program..."
-pacman -S --needed ${MINGW_PACKAGE_PREFIX}-toolchain
-pacman -S --needed vim git ${MINGW_PACKAGE_PREFIX}-python ${MINGW_PACKAGE_PREFIX}-python-pip tmux make
+pacman -S --needed ${MINGW_PACKAGE_PREFIX}-toolchain \
+	git ${MINGW_PACKAGE_PREFIX}-git-lfs \
+	${MINGW_PACKAGE_PREFIX}-cmake ${MINGW_PACKAGE_PREFIX}-ccmake \
+	${MINGW_PACKAGE_PREFIX}-make \
 
-echo "================== Finish =================="
-
-echo "Setup python enviroment..."
-python -m pip install --upgrade pip
-pip install virtualenv
 echo "================== Finish =================="
 
 echo "Setup .bashrc ..."
